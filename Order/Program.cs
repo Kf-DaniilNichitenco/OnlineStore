8574831cfdf42e9b6993de22d86e35aa5320ce2f
Daniil Nichitenco <daniil.nichitenco@amdaris.com>
2022-02-08 11:06:07 +0200

    Init commit

---
+++ Order/Program.cs
@@ -0,0 +1,32 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddFastEndpoints();
+builder.Services.AddSwaggerDoc(settings =>
+{
+    settings.Title = "Order service";
+    settings.Version = "v1";
+});
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+app.UseFastEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi3(s => s.ConfigureDefaults());
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
