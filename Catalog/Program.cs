906e5dd9179b17737390bcf573d12051e5524843
Daniil Nichitenco <daniil.nichitenco@amdaris.com>
2022-04-01 17:53:53 +0300

    Updated swagger mapping logic

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,5 +1,9 @@
+using System.Reflection;
+using Catalog;
 using FastEndpoints;
-using Microsoft.OpenApi.Models;
+using FastEndpoints.Swagger;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +13,11 @@ builder.Services.AddSwaggerDoc(settings =>
     {
         settings.Title = "Catalog service";
         settings.Version = "v1";
+        settings.SerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new RequireObjectPropertiesContractResolver(),
+        };
+        settings.OperationProcessors.Add(new CamelCaseQueryParamsOperationProcessor());
     },
     tagIndex: 2,
     shortSchemaNames: true);
@@ -30,3 +39,38 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.Run();
+
+internal class RequireObjectPropertiesContractResolver : CamelCasePropertyNamesContractResolver
+{
+    protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+    {
+        var property = base.CreateProperty(member, memberSerialization);
+        if (property.PropertyType != null
+            && Nullable.GetUnderlyingType(property.PropertyType) == null
+            && (!property.PropertyType.IsGenericType || property.PropertyType.GetGenericTypeDefinition() != typeof(Nullable<>)))
+        {
+            property.Required = Required.Always;
+        }
+        return property;
+    }
+    protected override JsonObjectContract CreateObjectContract(Type objectType)
+    {
+        var contract = base.CreateObjectContract(objectType);
+        if (Nullable.GetUnderlyingType(objectType) == null && (!objectType.IsGenericType || objectType.GetGenericTypeDefinition() != typeof(Nullable<>)))
+        {
+            contract.ItemRequired = Required.Always;
+        }
+        return contract;
+    }
+    protected override JsonProperty CreatePropertyFromConstructorParameter(JsonProperty? matchingMemberProperty, ParameterInfo parameterInfo)
+    {
+        var property = base.CreatePropertyFromConstructorParameter(matchingMemberProperty, parameterInfo);
+        if (property.PropertyType != null
+            && Nullable.GetUnderlyingType(property.PropertyType) == null
+            && (!property.PropertyType.IsGenericType || property.PropertyType.GetGenericTypeDefinition() != typeof(Nullable<>)))
+        {
+            property.Required = Required.Default;
+        }
+        return property;
+    }
+}
