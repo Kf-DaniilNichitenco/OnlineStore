560be147180a18d1e3d901f124a1e86a9a21e3de
Daniil Nichitenco <daniil.nichitenco@amdaris.com>
2022-04-14 16:01:31 +0300

    Updated login guard

--- Clients/OnlineStore/src/app/common/services/guards/login-guard.service.ts
+++ Clients/OnlineStore/src/app/common/services/guards/login-guard.service.ts
@@ -1,42 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { CanActivate, CanLoad } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginGuardService implements CanActivate, CanLoad {
+  public constructor(private _authService: AuthService) {}
 
-  public constructor(private _authService: AuthService) { }
-
-  canLoad(): Promise<boolean> {
-    return this.isUserNotAuthenticated();
+  public canActivate(): Observable<boolean> {
+    return this._authService.isAuthenticated();
   }
-
-  public canActivate(): Promise<boolean> {
-    return this.isUserNotAuthenticated();
-  }
-
-  private async isUserNotAuthenticated(): Promise<boolean> {
-    let isAuthenticated: boolean;
-
-    try {
-      isAuthenticated = await this._authService.isAuthenticated();
-    } catch(error: any) {
-      console.error(error);
-
-      isAuthenticated = false;
-    }
-
-    return !isAuthenticated;
-
-    // return this._authService.isAuthenticated()
-    // .then(res => !res)
-    // .catch(error => {
-    //   console.error(error);
-
-    //   return true;
-    // });
+  public canLoad(): Observable<boolean> {
+    return this._authService.isAuthenticated();
   }
 }
